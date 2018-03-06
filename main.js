cb6ef3ccd2c8a990b42a6dbc40e677ad401cc368
Natalia Petkevich <petkevich.natalli@gmail.com>
2018-03-07 00:49:08 +0300

    async

--- main.js
+++ main.js
@@ -1,39 +1,60 @@
-const readline = require('readline');
+const yargs = require('yargs');
 const findFiles = require('./lib/find-files.js');
 const copyFiles = require('./lib/copy-files.js');
 const deleteFiles = require('./lib/delete-files.js');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const util = require('util');
 
-rl.question('Имя исходной папки?', source => {
-  const array = findFiles(source, (error) => {
-    if (error) {
-      console.log(error);
-      process.exit(1);
+const argv = yargs
+  .usage('main [options]')
+  .demand('f')
+  .nargs('f', 3)
+  .describe('f', 'Укажите: имя исходной папки, имя новой папки, надо ли удалить исходную папку (yes/no)')
+  .alias('f', 'folder')
+  .argv;
+
+const phrase = argv.f;
+const source = phrase[0];
+const newFolder = phrase[1];
+const delFolder = phrase[2];
+
+async function main () {
+  try {
+    const array = await findFiles(source);
+    await copyFiles(newFolder, array);
+    if (/^y(es)?$/i.test(delFolder)) {
+      await deleteFiles(source);
+      console.log('Operation is completed. The source folder was deleted');
+      process.exit(0);
     }
-  });
-  rl.question('Имя новой папки?', (answer) => {
-    copyFiles(answer, source, array, (error) => {
-      if (error) {
-        console.log(error);
-        process.exit(1);
-      }
-    });
-    rl.question('Удалить старую папку? (Yes/No)?', (answer) => {
-      if (/^y(es)?$/i.test(answer)) {
-        deleteFiles(source, (error) => {
-          if (error) {
-            console.log(error);
-            process.exit(1);
-          }
-        });
-        console.log('Operation is completed. The source folder was deleted');
-        process.exit(0);
-      }
-      console.log('Operation is completed. The source folder is not deleted');
-      rl.close();
-    });
-  });
-});
+    console.log('Operation is completed. The source folder is not deleted');
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+main();
+// const array = findFiles(source, (error) => {
+//   if (error) {
+//     console.log(error);
+//     process.exit(1);
+//   }
+// });
+// // console.log(111, array);
+// copyFiles(newFolder, array, (error) => {
+//   if (error) {
+//     console.log(error);
+//     process.exit(1);
+//   }
+// });
+// if (/^y(es)?$/i.test(delFolder)) {
+//   deleteFiles(source, (error) => {
+//     if (error) {
+//       console.log(error);
+//       process.exit(1);
+//     }
+//   });
+//   console.log('Operation is completed. The source folder was deleted');
+//   process.exit(0);
+// }
+// console.log('Operation is completed. The source folder is not deleted');
+// // process.exit(0);
